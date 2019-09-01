92490bee3d2751c6bbaceb0f5aaba6209d0ffa75
Hemoptysis Heart <hemoptysisheart@gmail.com>
2019-09-01 12:08:05 +0900

    FAIL : 단순히 HTTP/2 활성화.
    설정만 활성화 하면 브라우저는 HTTP/1.1 프로토콜을 사용하는 걸 확인할 수 있다.

---
+++ grpc/src/main/java/com/github/hemoptysisheart/spring/example/grpc/runner/GrpcRunner.java
@@ -0,0 +1,23 @@
+package com.github.hemoptysisheart.spring.example.grpc.runner;
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author justburrow
+ * @since 2019/09/01
+ */
+@SpringBootApplication(scanBasePackages = "com.github.hemoptysisheart.spring.example.grpc")
+public class GrpcRunner {
+  private static final Logger log = getLogger(GrpcRunner.class);
+
+  public static void main(String... args) {
+    SpringApplication application = new SpringApplication(GrpcRunner.class);
+    application.addListeners(new ApplicationPidFileWriter());
+    application.run(args);
+  }
+}
