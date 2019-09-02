da00b004d9b0bb4844c60ed24e134c219df8f791
Hemoptysis Heart <hemoptysisheart@gmail.com>
2019-09-03 00:34:38 +0900

    ADD : npm code.

--- grpc/src/main/resources/files/js/grpc.js
+++ grpc/src/main/resources/files/js/grpc.js
@@ -1,8 +1,11 @@
-$(document).ready(function () {
-  console.log("jQuery ready!");
+const {PingRequest} = require('./ping/ping_request_pb');
+const {PingResponse} = require('./ping/ping_response_pb');
+const {PingServiceClient} = require('./ping/ping_service_grpc_web_pb');
 
-  $("#ping-bt").click(function (evt) {
-    console.debug("evt", evt)
-    alert("click!")
-  });
+let client = new PingServiceClient('http://localhost:5656');
+
+let request = new PingRequest();
+
+client.ping(request, {}, function (err, response) {
+  console.debug("err", err, "response", response);
 });
