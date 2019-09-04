639de65426784017612619aba9a8210c54767356
Hemoptysis Heart <hemoptysisheart@gmail.com>
2019-09-05 00:04:25 +0900

    UPDATE : let's encrypt 관련 내용 분리.

--- grpc/README.md
+++ grpc/README.md
@@ -1,2 +1,5 @@
 # gRPC
 
+## 참고
+
+- [gRPC Basics - Web](https://grpc.io/docs/tutorials/basic/web/)
--- grpc/compile_js.sh
+++ grpc/compile_js.sh
@@ -5,8 +5,20 @@ JS_OUT_DIR="src/main/resources/files/js/"
 
 [ -d $JS_OUT_DIR ] || mkdir -p $JS_OUT_DIR
 
+echo "before :"
+gls -l --time-style=full-iso $JS_OUT_DIR/ping
+rm -rf $JS_OUT_DIR/ping
+
 for PROTO in $(ls $PROTO_DIR/ping)
 do
     protoc -I=$PROTO_DIR ping/$PROTO --js_out=import_style=commonjs:$JS_OUT_DIR
     protoc -I=$PROTO_DIR ping/$PROTO --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$JS_OUT_DIR
 done
+
+echo
+echo "after :"
+gls -l --time-style=full-iso $JS_OUT_DIR/ping
+
+echo
+echo "git status :"
+git status $JS_OUT_DIR/ping
