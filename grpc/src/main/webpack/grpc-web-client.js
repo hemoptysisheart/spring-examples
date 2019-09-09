3f3cd3339421ac69883e1a96332d5e7887b4616e
Hemoptysis Heart <hemoptysisheart@gmail.com>
2019-09-10 00:57:52 +0900

    TEMP

--- grpc/src/main/webpack/grpc-web-client.js
+++ grpc/src/main/webpack/grpc-web-client.js
@@ -1,18 +1,18 @@
-const PingRequest = require("./ping/ping_request_pb.js");
-console.log("PingRequest", PingRequest);
+const {a} = require("./ping/ping_request_pb.js");
+console.log(a);
 
-const PingResponse = require("./ping/ping_response_pb.js");
-console.debug("PingResponse", PingResponse);
+const {b, c} = require("./ping/ping_response_pb.js");
+console.debug(b, c);
 
-const PingServiceClient = require("./ping/ping_service_grpc_web_pb.js");
-console.debug("PingServiceClient", PingServiceClient);
+const {d, e} = require("./ping/ping_service_grpc_web_pb.js");
+console.debug(d, e);
 
-const pingService = new PingServiceClient("http://localhost:6565");
-console.debug("pingService", pingService);
-
-let request = new PingRequest();
-console.debug("request", request);
-
-pingService.ping(request, {}, function (err, response) {
-  console.debug("pingService.ping callback", "err", err, "response", response);
-});
+// const pingService = new PingServiceClient("http://localhost:6565");
+// console.debug("pingService", pingService);
+//
+// let request = new PingRequest();
+// console.debug("request", request);
+//
+// pingService.ping(request, {}, function (err, response) {
+//   console.debug("pingService.ping callback", "err", err, "response", response);
+// });
