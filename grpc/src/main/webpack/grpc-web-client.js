68c5ea6db0d77ef8a69715623c370e47982b4afa
Hemoptysis Heart <hemoptysisheart@gmail.com>
2019-09-10 01:06:03 +0900

    Revert "TEMP"
    This reverts commit 3f3cd333

--- grpc/src/main/webpack/grpc-web-client.js
+++ grpc/src/main/webpack/grpc-web-client.js
@@ -1,18 +1,18 @@
-const {a} = require("./ping/ping_request_pb.js");
-console.log(a);
+const PingRequest = require("./ping/ping_request_pb.js");
+console.log("PingRequest", PingRequest);
 
-const {b, c} = require("./ping/ping_response_pb.js");
-console.debug(b, c);
+const PingResponse = require("./ping/ping_response_pb.js");
+console.debug("PingResponse", PingResponse);
 
-const {d, e} = require("./ping/ping_service_grpc_web_pb.js");
-console.debug(d, e);
+const PingServiceClient = require("./ping/ping_service_grpc_web_pb.js");
+console.debug("PingServiceClient", PingServiceClient);
 
-// const pingService = new PingServiceClient("http://localhost:6565");
-// console.debug("pingService", pingService);
-//
-// let request = new PingRequest();
-// console.debug("request", request);
-//
-// pingService.ping(request, {}, function (err, response) {
-//   console.debug("pingService.ping callback", "err", err, "response", response);
-// });
+const pingService = new PingServiceClient("http://localhost:6565");
+console.debug("pingService", pingService);
+
+let request = new PingRequest();
+console.debug("request", request);
+
+pingService.ping(request, {}, function (err, response) {
+  console.debug("pingService.ping callback", "err", err, "response", response);
+});
