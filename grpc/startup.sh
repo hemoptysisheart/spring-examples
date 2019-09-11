d31677123e660bdb42b611d50a56f8a8edec5795
Hemoptysis Heart <hemoptysisheart@gmail.com>
2019-09-11 23:35:32 +0900

    ADD : utility scripts.

--- grpc/startup.sh
+++ grpc/startup.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-cd envoy
+REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
+EXAMPLE_ROOT="$REPOSITORY_ROOT/grpc"
+echo "REPOSITORY ROOT PATH : $REPOSITORY_ROOT"
+echo "EXAMPLE ROOT PATH : $EXAMPLE_ROOT"
+echo
+
+# envoy proxy   #######################################################################################################
+cd ${EXAMPLE_ROOT}/envoy
 docker build -t helloworld/envoy .
+echo
 docker run -d --rm -p 8080:8080 -p 9901:9901 --network=host --name grpc_envoy helloworld/envoy
+echo
 docker container ls --all
+
+# spring boot   #######################################################################################################
+BOOT_JAR_DIR="$EXAMPLE_ROOT/build/libs"
+
+cd ${EXAMPLE_ROOT}
+./gradlew bootJar
+cd ${BOOT_JAR_DIR}
+./grpc-0.0.1.SNAPSHOT.jar &> /dev/null &
+
+SPRING_BOOT_PID=$(cat ${BOOT_JAR_DIR}/application.pid)
+echo
+echo "SPRING BOOT PID : $SPRING_BOOT_PID"
--- grpc/stop.sh
+++ grpc/stop.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
-cd envoy
+REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
+EXAMPLE_ROOT="$REPOSITORY_ROOT/grpc"
+echo "REPOSITORY ROOT PATH : $REPOSITORY_ROOT"
+echo "EXAMPLE ROOT PATH : $EXAMPLE_ROOT"
+echo
+
+# envoy proxy   #######################################################################################################
+cd ${EXAMPLE_ROOT}/envoy
 docker container stop grpc_envoy
+echo
 docker container ls --all
+
+# spring boot   #######################################################################################################
+BOOT_JAR_DIR="$EXAMPLE_ROOT/build/libs"
+SPRING_BOOT_PID=$(cat ${BOOT_JAR_DIR}/application.pid)
+
+echo
+cd $BOOT_JAR_DIR
+echo "SPRING BOOT PID : $SPRING_BOOT_PID"
+kill -9 ${SPRING_BOOT_PID}
