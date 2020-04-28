5d5864413a227d1f73b663a4fbab52ea77dee9fd
Hemoptysis Heart <hemoptysisheart@gmail.com>
2020-04-28 23:37:53 +0900

    ADD : test case.

--- jpa/daily/src/test/java/com/hemoptysisheart/spring/examples/jpa/daily/repository/PersonRepositoryTest.java
+++ jpa/daily/src/test/java/com/hemoptysisheart/spring/examples/jpa/daily/repository/PersonRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.hemoptysisheart.spring.examples.jpa.daily.repository;
 
 import com.hemoptysisheart.spring.examples.jpa.daily.JpaDailyTestConfiguration;
+import com.hemoptysisheart.spring.examples.jpa.daily.entity.Diary;
 import com.hemoptysisheart.spring.examples.jpa.daily.entity.Person;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +29,11 @@ public class PersonRepositoryTest {
 
   @Autowired
   private PersonRepository repository;
+  @Autowired
+  private DiaryRepository diaryRepository;
+
+  @Autowired
+  private EntityManager entityManager;
 
   @Test
   public void test_findAll() throws Exception {
@@ -54,5 +62,32 @@ public class PersonRepositoryTest {
         .isNotNull()
         .extracting(Person::getName)
         .isEqualTo(name);
+  }
+
+  @Test
+  void test_load() {
+    // GIVEN
+    Person person = this.repository.save(new Person("person"));
+    log.info("GIVEN - person={}", person);
+
+    Diary diary = this.diaryRepository.save(new Diary(person, LocalDate.now(), "content"));
+    log.info("GIVEN - diary={}", diary);
+
+    this.entityManager.flush();
+    this.entityManager.clear();
+
+    // WHEN
+    Person actual = this.repository.findById(person.getId()).get();
+    log.info("WHEN - actual={}", actual);
+
+    // THEN
+    assertThat(actual)
+        .isNotNull()
+        .isEqualTo(person)
+        .isNotSameAs(person);
+    assertThat(actual.getDiary(LocalDate.now()))
+        .isNotNull()
+        .extracting(Diary::getId, Diary::getDate, Diary::getContent)
+        .containsSequence(diary.getId(), LocalDate.now(), "content");
   }
 }
